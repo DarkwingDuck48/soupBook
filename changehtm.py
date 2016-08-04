6372ce200d0d4a6a38fe2b7a20baa32dafa1db42
Max Britvin <maksbritvin@gmail.com>
2016-08-04 19:37:24 +0300

    Почти все, кроме атрибута background

--- changehtm.py
+++ changehtm.py
@@ -1,30 +1,54 @@
 import os,stat
 from bs4 import BeautifulSoup
-import re
 
 
 pattern_bg = "white.gif"
-pattern_book = "turnbook.gif"
-path_work = "D:\Документы\Книга_Геодезия_Дьяков\Example"
-workdir = os.listdir(path_work)
+path_work = "D:\Книга\Example"
+workdir = [direct for direct in os.listdir(path_work) if os.path.isfile(path_work+"\\"+direct)]
+print(workdir)
 for direct in workdir:
-    if os.path.isfile(path_work+"\\"+direct):
-        path_to_file = path_work+"\\"+direct
-        if os.access(path_to_file,os.R_OK):
-            os.chmod(path_to_file, stat.S_IRWXU)
-        file = open(path_to_file, "r+", encoding='windows-1251')
-        soup = BeautifulSoup(file, "lxml", from_encoding="windows-1251")
-        links = soup.find_all("a")
-        images = soup.find_all("img")
-        print(path_to_file)
-        for link in links:
-            print(link.string)
-        for image in images:
-            if image['src'].endswith(pattern_book):
-                image['src'] = "Files/book/turnbook.gif"
-            print(image['src'])
-        print(type(soup.prettify()))
-        file2 = open(path_to_file, "w+", encoding='utf-8')
-        file2.write(soup.prettify())
-        file2.close()
+    path_to_file = path_work+"\\"+direct
+    if os.access(path_to_file, os.R_OK):
+        os.chmod(path_to_file, stat.S_IRWXU)
+    file = open(path_to_file, "r+", encoding='windows-1251')
+    soup = BeautifulSoup(file, "lxml", from_encoding="windows-1251")
+    links = soup.find_all("a")
+    images = soup.find_all("img")
+    print("----------------------------------------------------------------------------------------------------------")
+    print("Изменяемый файл - ", path_to_file)
+    for link in links:
+        if link.string == "Об авторах" and link['href'] != "0002_Об_авторах.htm":
+            link['href'] = "0002_Об_авторах.htm"
+        elif link.string == "Содержание" and link['href'] != "0003_Содержание.htm":
+            link['href'] = "0003_Содержание.htm"
+        elif str(link.string).startswith("Предыдущий раздел") or str(link.string).startswith("Следующий раздел"):
+            if str(link.string).startswith("Предыдущий раздел"):
+                print("This is previous file for {1} is {0}".format(workdir[workdir.index(direct)-1], direct))
+                link['href'] = workdir[workdir.index(direct)-1]
+            elif str(link.string).startswith("Следующий раздел"):
+                if workdir.index(direct) + 1 < len(workdir):
+                    print("This is next file for {1} is {0}".format(workdir[workdir.index(direct) + 1], direct))
+                    link['href'] = workdir[workdir.index(direct) + 1]
+                else:
+                    print("Files finish.")
+                    link['href'] = workdir[0]
+        print(link)
+    print("----------------------------------------------------------------------------------------------------------")
+    for image in images:
+        if image['src'].endswith("turnbook.gif"):
+            image['src'] = "Files/book/turnbook.gif"
+        elif image['src'].endswith('left.gif'):
+            image['src'] = "Files/hand/left.gif"
+        elif image['src'].endswith('right.gif'):
+            image['src'] = "Files/hand/right.gif"
+        else:
+            folder = direct.split(".")
+            #0073_Геодезия_6_1
+            filename = image['src'].split("/")
+            todebug = "Files/pages/"+folder[0][14:]+"/"+filename[-1]
+            image['src'] = "Files/pages/"+folder[0][14:]+"/"+filename[-1]
+            #print(image['src'])
+    file2 = open(path_to_file, "w+", encoding='utf-8')
+    file2.write(soup.prettify())
+    file2.close()
 
